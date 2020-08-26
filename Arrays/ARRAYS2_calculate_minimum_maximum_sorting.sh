bb3ef56e72c5fe5f47b28ec1f35830121be6e55c
myramoon <68750876+myramoon@users.noreply.github.com>
2020-08-26 13:17:26 +0530

    Programs based on Arrays

--- Arrays/ARRAYS2_calculate_minimum_maximum_sorting.sh
+++ Arrays/ARRAYS2_calculate_minimum_maximum_sorting.sh
@@ -6,50 +6,50 @@
 declare -a random	#for generating 10 random 3 digit numbers
 
 for (( count=0; count<=9; count++ ))
-	do
-		random[(($count))]=$(( RANDOM%900 + 100 ))
-	done
+do
+	random[(($count))]=$(( RANDOM%900 + 100 ))
+done
 
 echo "UNSORTED:" ${random[@]}
 
 
 for (( i=0; i<=8; i++  ))
+do
+	min_index=$i
+	for (( j=$(( $i + 1)); j<=9; j++ ))
 	do
-		min_index=$i
-		for (( j=$(( $i + 1)); j<=9; j++ ))
-		do
 		if [ ${random[(($j))]} -lt ${random[(($min_index))]} ]
 		then
         		min_index=$j
 		fi
-		done
-
-		temp=${random[(($i))]}
-		random[(($i))]=${random[((min_index))]}    #min number is at leftmost position
-		random[((min_index))]=$temp                #the larger number is placed in mins position
-
 	done
+
+	temp=${random[(($i))]}
+	random[(($i))]=${random[((min_index))]}    #min number is at leftmost position
+	random[((min_index))]=$temp                #the larger number is placed in mins position
+
+done
 
 echo "Sorted" ${random[@]}
 echo "Second min" ${random[1]}
 
 for (( i=0; i<=8; i++ ))
-	do
+do
 	max_index=$i
 
 	for(( j=$(( $i + 1 )); j<=9; j++ ))
-		do
+	do
 		if [ ${random[(($j))]} -gt ${random[(($max_index))]} ]
 		then
 			max_index=$j
 		fi	
-		done
+	done
 
 	temp=${random[(($i))]}
 	random[(($i))]=${random[(($max_index))]}
 	random[(($max_index))]=$temp
 
-	done
+done
 
 echo "Sorted descending: " ${random[@]}
 echo "Second max" ${random[1]}
